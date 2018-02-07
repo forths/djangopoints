0001394241ddf032a512f7984549c9825f09f4f3
wangshaoyang <wangsy@deepait.com>
2018-02-07 20:26:05 +0800

    static change

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6^v$0&acr^##9444d70g2k25+b=2zi^@@+lx4_p!z_cbq6d(zq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
